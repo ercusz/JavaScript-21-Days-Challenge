a83a988f6549fdef0f47aebb7a07d453bb8b3f31
ercusz <57826174+ercusz@users.noreply.github.com>
2022-09-13 22:39:21 +0700

    finish day 7

--- 07 - Konami Code/start.js
+++ 07 - Konami Code/start.js
@@ -1,4 +1,36 @@
 (() => {
   // เริ่มเขียนโค้ด
+  const KONAMI_CODE = [
+    "ArrowUp",
+    "ArrowUp",
+    "ArrowDown",
+    "ArrowDown",
+    "ArrowLeft",
+    "ArrowRight",
+    "ArrowLeft",
+    "ArrowRight",
+    "b",
+    "a",
+  ];
 
+  let idx = 0;
+
+  onKeyDown = (e) => {
+    // use regex for insensitive string comparison
+    // can also use both .toUpperCase() OR both .toLowerCase()
+    // OR a.localeCompare(b, undefined, { sensitivity: 'accent' }) === 0
+    new RegExp(KONAMI_CODE[idx], "i").test(e.key) ? idx++ : (idx = 0);
+
+    console.log(e.key);
+
+    if (idx === KONAMI_CODE.length) {
+      startSnowing();
+    }
+  };
+
+  run = () => {
+    document.addEventListener("keydown", onKeyDown);
+  };
+
+  run();
 })();
