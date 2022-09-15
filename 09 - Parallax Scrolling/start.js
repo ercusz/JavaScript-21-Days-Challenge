08bb55c9fd17101a636b5d7a336d08f673d3c87c
ercusz <57826174+ercusz@users.noreply.github.com>
2022-09-15 17:29:51 +0700

    finish day 9

--- 09 - Parallax Scrolling/start.js
+++ 09 - Parallax Scrolling/start.js
@@ -1,4 +1,18 @@
 (() => {
   // เริ่มเขียนโค้ด
+  onScroll = () => {
+    const moonElem = document.querySelector(".moon");
+    const wishElem = document.querySelector(".wish");
 
+    moonElem.style.transform = `translate(${window.scrollY * 0.7}%, ${
+      window.scrollY * -0.7
+    }%)`;
+    wishElem.style.transform = `translateY(${window.scrollY * -1.2}%)`;
+  };
+
+  run = () => {
+    window.addEventListener("scroll", onScroll);
+  };
+
+  run();
 })();
