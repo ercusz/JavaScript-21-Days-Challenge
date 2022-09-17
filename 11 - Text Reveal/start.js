24b491a8a521fcc7b627991fd6e26ce7affa1c71
ercusz <57826174+ercusz@users.noreply.github.com>
2022-09-17 16:28:56 +0700

    finish day 11

--- 11 - Text Reveal/start.js
+++ 11 - Text Reveal/start.js
@@ -1,4 +1,24 @@
 (() => {
-  // เริ่มเขียนโค้ด
+  onScroll = () => {
+    const sectionElems = Array.from(document.querySelectorAll("section"));
 
+    sectionElems.forEach((sectionElem) => {
+      const imgElem = sectionElem.querySelector("img");
+      const textElem = sectionElem.querySelector(".text");
+
+      const revealPosition = imgElem.offsetTop + imgElem.offsetHeight / 10;
+      const scrollPosition = window.pageYOffset;
+
+      if (scrollPosition >= revealPosition) {
+        textElem.classList.add("reveal");
+      }
+    });
+  };
+
+  // เริ่มเขียนโค้ด
+  run = () => {
+    document.addEventListener("scroll", onScroll);
+  };
+
+  run();
 })();
