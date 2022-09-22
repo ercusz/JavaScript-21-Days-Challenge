28cf36e31057de1c079fddb7af59a9e0fb6527fe
ercusz <57826174+ercusz@users.noreply.github.com>
2022-09-22 19:07:44 +0700

    finish day 16

--- 16 - Text to Speech/start.js
+++ 16 - Text to Speech/start.js
@@ -1,4 +1,28 @@
 (() => {
   // เริ่มเขียนโค้ด
+  const message = new SpeechSynthesisUtterance();
 
+  onVoicesChanged = () => {
+    const voices = speechSynthesis.getVoices();
+    const thVoice = voices.find((voice) => voice.lang === "th-TH");
+    // thVoice will be undefined, if the Thai voice package is not installed in your OS
+    message.voice = thVoice;
+    console.log(voices);
+  };
+
+  onClick = (e) => {
+    message.text = e.target.getAttribute("alt");
+    speechSynthesis.speak(message);
+  };
+
+  run = () => {
+    speechSynthesis.addEventListener("voiceschanged", onVoicesChanged);
+
+    const imgElems = Array.from(document.querySelectorAll("img"));
+    imgElems.forEach((imgElem) => {
+      imgElem.addEventListener("click", onClick);
+    });
+  };
+
+  run();
 })();
