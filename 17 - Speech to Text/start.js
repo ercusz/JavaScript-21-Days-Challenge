19b0de062306b7e056a3c1231e6ba05851291c40
ercusz <57826174+ercusz@users.noreply.github.com>
2022-09-23 15:25:15 +0700

    finish day 17

--- 17 - Speech to Text/start.js
+++ 17 - Speech to Text/start.js
@@ -1,3 +1,46 @@
 (() => {
   // เริ่มเขียนโค้ด
+  window.SpeechRecognition =
+    window.SpeechRecognition || window.webkitSpeechRecognition;
+
+  const recognition = new window.SpeechRecognition();
+  const controlBtnElem = document.querySelector(".control");
+
+  isPause = () => {
+    return controlBtnElem.classList.contains("record");
+  };
+
+  onEnd = () => {
+    if (!isPause()) {
+      recognition.start();
+    }
+  };
+
+  onResult = (e) => {
+    const textElem = document.querySelector(".text");
+    const { transcript } = e.results[0][0];
+    textElem.innerText += transcript;
+  };
+
+  onClick = (e) => {
+    if (isPause()) {
+      recognition.start();
+      e.target.classList.remove("record");
+      e.target.classList.add("pause");
+    } else {
+      recognition.stop();
+      e.target.classList.remove("pause");
+      e.target.classList.add("record");
+    }
+  };
+
+  run = () => {
+    recognition.lang = "th-TH";
+    recognition.addEventListener("end", onEnd);
+    recognition.addEventListener("result", onResult);
+
+    controlBtnElem.addEventListener("click", onClick);
+  };
+
+  run();
 })();
